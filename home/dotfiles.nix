0da1c9f2a7fb4e1eb953f86ad8811ebc8363e91f
Raquel <ela@raquellie.com>
2026-04-27 23:00:46 +0100

    Reduced the dependency on the DOTFILES envvar

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,31 +1,31 @@
 {
-  inputs,
-  config,
-  pkgs,
-  lib,
-  ...
+	inputs,
+		config,
+		pkgs,
+		lib,
+		...
 }:
 
 {
-  # HM sets a guard against these being sourced more than once per login which makes quick tweaks maddening
-  home = {
-    sessionVariables = {
-      DOTFILES = "${inputs.dotfiles}";
+# HM sets a guard against these being sourced more than once per login which makes quick tweaks maddening
+	home = {
+		sessionVariables = {
+			VIMINIT = "source ${config.xdg.configHome}/vim/vimrc";
+		};
 
-      VIMINIT = "source ${inputs.dotfiles}/config/vim/vimrc";
-      STARSHIP_CONFIG = "${inputs.dotfiles}/config/starship.toml";
-    };
+		sessionPath = [
+			"${inputs.dotfiles}/environment/path"
+		];
+	};
 
-    sessionPath = [
-      "${inputs.dotfiles}/environment/path"
-    ];
-  };
+	programs.zsh.initContent = lib.fileContents ("${inputs.dotfiles}/config/zsh/.zshrc");
 
-  programs.zsh.initContent = lib.fileContents ("${inputs.dotfiles}/config/zsh/.zshrc");
+	xdg.configFile = {
+		"vim".source = "${inputs.dotfiles}/config/vim";
+		"kitty".source = "${inputs.dotfiles}/config/kitty";
+		"ranger".source = "${inputs.dotfiles}/config/ranger";
+		"gdb".source = "${inputs.dotfiles}/config/gdb";
 
-  xdg.configFile = {
-    "kitty".source = "${inputs.dotfiles}/config/kitty";
-    "ranger".source = "${inputs.dotfiles}/config/ranger";
-    "gdb".source = "${inputs.dotfiles}/config/gdb";
-  };
+		"starship.toml".source = "${inputs.dotfiles}/config/starship.toml";
+	};
 }
