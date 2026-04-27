0e196f57d92abdbb37b1ae0c1e4a7bbc54db362c
Raquel <dev@raquellie.com>
2026-04-27 23:14:30 +0100

    More tweaks!

--- desktop/gaming.nix
+++ desktop/gaming.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+
+  # For better perfomance in Wine/Proton
+  boot.kernelModules = [ "ntsync" ];
+
   programs.steam = {
     enable = false;
     remotePlay.openFirewall = true;
@@ -9,10 +13,18 @@
     ];
   };
 
-  services.wivrn.enable = false;
+  services = {
+    wivrn.enable = false;
+
+    # Network throughput testing utility - useful for checking there is enough bandwidth for VR
+    iperf3 = {
+      enable = false;
+      openFirewall = true;
+    };
+  };
 
   environment.systemPackages = with pkgs; [
-    #lutris
+    #faugus-launcher
     #steam-run
 
     #prismlauncher
