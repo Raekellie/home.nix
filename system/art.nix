4cbfc1255f45a962003ed74fe6805c560f568fb2
Raquel <ela@raquellie.com>
2026-08-05 19:08:36 +0100

    Patched in support for my drawing tablet
    With lots of help (and code written by) an OTD developer (Mrcubix @
    github), added in support for my tablet's inner wheel, which is a
    trackpad. Still missing its alternate mouse mode, which will probably
    need more messing with the inner workings of OTD (disabled libinput,
    which is necessary for the mouse to work).
    Old patches kept in just in case, to be removed later.

--- system/art.nix
+++ system/art.nix
@@ -1,14 +1,36 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  hardware.opentabletdriver.enable = true;
+  programs.xppen.enable = false;
+  nixpkgs.config.allowUnfreePackages = [
+    "xppen_4"
+  ];
+  hardware.opentabletdriver = {
+    enable = true;
+    package = pkgs.patched-opentabletdriver;
+  };
+  # TODO: remove OTD patches when upstreamed - adds support for the inner trackpad wheel
+  # Patches written by https://github.com/Mrcubix
+  nixpkgs.overlays = [
+    (final: prev: {
+      patched-opentabletdriver = prev.opentabletdriver.overrideAttrs (previousAttrs: {
+        doCheck = false;
+        patches =
+          (previousAttrs.patches or [])
+          ++ [
+            ../custom/patches/otd-0001-trackpad-wheel.patch
+          ];
+      });
+    })
+  ];
 
   environment.systemPackages = with pkgs; [
     krita
-    digikam
-    pkgsRocm.blender
+    pkgsRocm.blender # FIXME: don't assume AMD GPU
+    #digikam
     #aseprite
 
     kdePackages.kdenlive
