ca79b730ccb4e15d7dc6db934d95d7666405fda0
Raquel <ela@raquellie.com>
2026-09-07 20:51:05 +0100

    Added a persist path for Technitium
    Lots of tracked changes despite not having changed all that much due to
    the formatter change to use tabs

--- hosts/biglab/default.nix
+++ hosts/biglab/default.nix
@@ -1,64 +1,65 @@
 {
-  config,
-  pkgs,
-  ...
+	config,
+	pkgs,
+	lib,
+	...
 }: {
-  imports = [
-    ./hardware-configuration.nix
-    ../../custom/impermanence.nix
-    ../../system/common.nix
-    ../../system/raquel.nix
-    ../../system/fish.nix
-    ../../system/utilities.nix
-    ../../services/ssh.nix
+	imports = [
+		./hardware-configuration.nix
+		../../custom/impermanence.nix
+		../../system/common.nix
+		../../system/raquel.nix
+		../../system/fish.nix
+		../../system/utilities.nix
+		../../services/ssh.nix
 
-    #../../services/vpn.nix
-    #../../services/caddy.nix
-    #../../services/authelia.nix
-    #../../services/zabbix.nix
-    #../../services/silverbullet.nix
-    #../../services/firefly-iii.nix
-    #../../services/technitium.nix
-    #../../services/pufferpanel.nix
-  ];
+		../../services/technitium.nix
+		#../../services/caddy.nix
+		#../../services/authelia.nix
+		#../../services/zabbix.nix
+		#../../services/silverbullet.nix
+		#../../services/firefly-iii.nix
+		#../../services/pufferpanel.nix
+	];
 
-  custom.impermanence = {
-    enable = true;
-    persistPath = "/persist";
+	custom.impermanence = {
+		enable = true;
+		persistPath = "/persist";
 
-    btrfs = {
-      enable = true;
-      rootDevice = config.fileSystems."/".device;
-      rootSubvol = "root";
-      daysToKeep = 14;
-    };
-  };
+		btrfs = {
+			enable = true;
+			rootDevice = config.fileSystems."/".device;
+			rootSubvol = "root";
+			daysToKeep = 14;
+		};
+	};
 
-  boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+	boot = {
+		kernelPackages = pkgs.linuxPackages_latest;
 
-    loader = {
-      timeout = 0;
-      systemd-boot = {
-        enable = true;
-        editor = false;
-        configurationLimit = 5;
-        memtest86.enable = true;
-        #netbootxyz.enable = true; # Reminder for self that this is an option
-      };
-    };
-  };
+		loader = {
+			timeout = 0;
+			systemd-boot = {
+				enable = true;
+				editor = false;
+				configurationLimit = 5;
+				memtest86.enable = true;
+				#netbootxyz.enable = true; # Reminder for self that this is an option
+			};
+		};
+	};
 
-  networking = {
-    hostName = "biglab";
-    networkmanager.enable = true;
-  };
+	networking = {
+		hostName = "biglab";
+		networkmanager.enable = true;
+		wireless.enable = lib.mkForce false;
+	};
 
-  services = {
-    fail2ban.enable = true;
-  };
+	services = {
+		fail2ban.enable = true;
+	};
 
-  # Read the docs/release notes before changing this value
-  # (man configuration.nix, https://nixos.org/nixos/options.html, https://nixos.org/manual/nixos/stable/release-notes)
-  system.stateVersion = "26.05";
+	# Read the docs/release notes before changing this value
+	# (man configuration.nix, https://nixos.org/nixos/options.html, https://nixos.org/manual/nixos/stable/release-notes)
+	system.stateVersion = "26.05";
 }
--- services/technitium.nix
+++ services/technitium.nix
@@ -1,6 +1,14 @@
-{...}: {
-  services.technitium-dns-server = {
-    enable = true;
-    openFirewall = true;
-  };
+{config, ...}: let
+	impermanence = config.custom.impermanence;
+in {
+	services.technitium-dns-server = {
+		enable = true;
+		openFirewall = true;
+	};
+
+	environment.persistence."${impermanence.persistPath}" = {
+		directories = [
+			"/var/lib/technitium-dns-server"
+		];
+	};
 }
