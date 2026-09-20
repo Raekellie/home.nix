3e8bb43356a977414f4849280bf4158c0d1d1716
Raquel <ela@raquellie.com>
2026-09-20 23:35:02 +0100

    biglab: first steps in integrating script with nix
    Missing:
    - cleaner SOPS organisation
    - writing the systemd service unit for the script
    - telling said unit how to get the header files

--- sops/system.nix
+++ sops/system.nix
@@ -1,15 +1,24 @@
 {inputs, ...}: {
-  imports = [
-    inputs.sops-nix.nixosModules.sops
-  ];
+	imports = [
+		inputs.sops-nix.nixosModules.sops
+	];
 
-  sops = {
-    defaultSopsFile = ./secrets.yaml; # This will be dropped in the nix store
+	sops = {
+		defaultSopsFile = ./secrets.yaml; # This will be dropped in the nix store
 
-    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+		age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 
-    secrets = {
-      initialHashedPassword = {neededForUsers = true;};
-    };
-  };
+		secrets = {
+			initialHashedPassword = {neededForUsers = true;};
+
+			biglab_ddns_headers_router = {
+				format = "binary";
+				sopsFile = ./headers_router.enc;
+			};
+			biglab_ddns_headers_porkbun = {
+				format = "binary";
+				sopsFile = ./headers_porkbun.enc;
+			};
+		};
+	};
 }
